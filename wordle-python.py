b01a0a19e4025fa6ed1d369f7cef000be1463200
ntaimur <naimt515@deltalearns.ca>
2026-02-03 13:12:23 -0800

    Finished core
    Made the core code for the game

--- wordle-python.py
+++ wordle-python.py
@@ -18,11 +18,41 @@ word_list = ["apple", "grape", "chair", "spice", "track",
 print("\nWelcome to Wordle 2.0!")
 print("Guess the 5 letter word in 6 tries or less...")
 
-
 # pick a random word
 word = random.choice(word_list)
 
-print(word)
+# number of attempts
+attempts = 1
+
+
+while attempts <= 6:
+    guess = input(f"\nAttempt {attempts}: ").lower().strip()
+
+    if len(guess) != 5:
+        print("Your guess must be 5 letters. ")
+        continue
+
+    attempts += 1
+
+    guess_result = ""
+
+    for i in range(5):
+        if guess[i] == word[1]:
+            guess_result += "🟩"
+        elif guess[i] in word:
+            guess_result += "🟨"
+        else:
+            guess_result += "🟥"
+
+    print(guess_result, "\n")
+
+    if guess == word:
+        print("You guessed the word!")
+        break
+else:
+    print(f"\nYou're out of attempts! The word was {word}!")
+
+
 
 
 
