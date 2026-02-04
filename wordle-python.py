9f231f6c7e3ae6d7653e2ef140d91629bb134b52
ntaimur <naimt515@deltalearns.ca>
2026-02-04 14:13:33 -0800

    Added difficulties fully
    Finished option to choose your difficulty as well as a full game loop allowing you to play as much as you want.

--- wordle-python.py
+++ wordle-python.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import random
+gameLoop = True
+
 
 
 # list of words 
@@ -28,140 +30,168 @@ word_list_six = [
     "ribbon","shower","throne","unfold","vacuum","window","wonder","yogurt","zipper","bridge"
 ]
 
+while gameLoop: # main game loop
+    
+    gameLoop = False # stop loop when game starts
 
 
-# welcome
-print("\nWelcome to Wordle 2.0!")
+    # funcs for all difficulties
+    def easy_diff():
+
+        print("\nGuess the 4 letter word in 6 tries or less...")
+
+        # pick a random word
+        word = random.choice(word_list_four)
+
+        # number of attempts
+        attempts = 1
 
 
+        while attempts <= 6:
+            guess = input(f"\nAttempt {attempts}: ").lower().strip() # ask for user guess
+
+            if len(guess) != 4: # if guess isnt 4 letters
+                print("Your guess must be 4 letters. ") 
+                continue 
+            elif guess.isdigit(): # if guess is a number
+                print("Your guess must not have any numbers! ")
+                continue
 
 
-def easy_diff():
+            attempts += 1 # increase attempt by 1 if valid guess
 
-    print("Guess the 4 letter word in 6 tries or less...")
+            guess_result = "" # empty string
 
-    # pick a random word
-    word = random.choice(word_list_four)
+            for i in range(4): # go thru each letter in the guess and the word
+                if guess[i] == word[i]:
+                    guess_result += "🟩"  # if correct position  
+                elif guess[i] in word:
+                    guess_result += "🟨" # incorrect position but word still has letter
+                else:
+                    guess_result += "🟥" # word doesnt have letter
 
-    # number of attempts
-    attempts = 1
+            print(guess_result, "\n") # print the boxes 
+
+            if guess == word:
+                print("You guessed the word!") # if u guess the word
+                break
+        else:
+            print(f"\nYou're out of attempts! The word was {word}!") # ran out of attempts
+
+    def medium_diff():
+
+        print("\nGuess the 5 letter word in 6 tries or less...")
+
+        # pick a random word
+        word = random.choice(word_list_five)
+
+        # number of attempts
+        attempts = 1
 
 
-    while attempts <= 6:
-        guess = input(f"\nAttempt {attempts}: ").lower().strip() # ask for user guess
+        while attempts <= 6:
+            guess = input(f"\nAttempt {attempts}: ").lower().strip() # ask for user guess
 
-        if len(guess) != 4: # if guess isnt 4 letters
-            print("Your guess must be 5 letters. ") 
-            continue 
-        elif guess.isdigit(): # if guess is a number
-            print("Your guess must not have any numbers! ")
-            continue
+            if len(guess) != 5: # if guess isnt 5 letters
+                print("Your guess must be 5 letters. ") 
+                continue 
+            elif guess.isdigit(): # if guess is a number
+                print("Your guess must not have any numbers! ")
+                continue
 
 
-        attempts += 1 # increase attempt by 1 if valid guess
+            attempts += 1 # increase attempt by 1 if valid guess
 
-        guess_result = "" # empty string
+            guess_result = "" # empty string
 
-        for i in range(4): # go thru each letter in the guess and the word
-            if guess[i] == word[1]:
-                guess_result += "🟩"  # if correct position  
-            elif guess[i] in word:
-                guess_result += "🟨" # incorrect position but word still has letter
-            else:
-                guess_result += "🟥" # word doesnt have letter
+            for i in range(5): # go thru each letter in the guess and the word
+                if guess[i] == word[i]:
+                    guess_result += "🟩"  # if correct position  
+                elif guess[i] in word:
+                    guess_result += "🟨" # incorrect position but word still has letter
+                else:
+                    guess_result += "🟥" # word doesnt have letter
 
-        print(guess_result, "\n") # print the boxes 
+            print(guess_result, "\n") # print the boxes 
 
-        if guess == word:
-            print("You guessed the word!") # if u guess the word
+            if guess == word:
+                print("You guessed the word!") # if u guess the word
+                break
+        else:
+            print(f"\nYou're out of attempts! The word was {word}!") # ran out of attempts
+
+    def hard_diff():
+
+        print("\nGuess the 6 letter word in 6 tries or less...")
+
+        # pick a random word
+        word = random.choice(word_list_six)
+
+        # number of attempts
+        attempts = 1
+
+
+        while attempts <= 6:
+            guess = input(f"\nAttempt {attempts}: ").lower().strip() # ask for user guess
+
+            if len(guess) != 6: # if guess isnt 6 letters
+                print("Your guess must be 6 letters. ") 
+                continue 
+            elif guess.isdigit(): # if guess is a number
+                print("Your guess must not have any numbers! ")
+                continue
+
+
+            attempts += 1 # increase attempt by 1 if valid guess
+
+            guess_result = "" # empty string
+
+            for i in range(6): # go thru each letter in the guess and the word
+                if guess[i] == word[i]:
+                    guess_result += "🟩"  # if correct position  
+                elif guess[i] in word:
+                    guess_result += "🟨" # incorrect position but word still has letter
+                else:
+                    guess_result += "🟥" # word doesnt have letter
+
+            print(guess_result, "\n") # print the boxes 
+
+            if guess == word:
+                print("You guessed the word!") # if u guess the word
+                break
+        else:
+            print(f"\nYou're out of attempts! The word was {word}!") # ran out of attempts
+
+    # func for playing again
+    def game_loop_func():
+        print("\nWant to play again?")
+        playAgain = input("Play again? (Y/N): ").lower().strip()
+        if playAgain == "y":
+            gameLoop = True # restart main game loop
+        elif playAgain == "n": # if no
+            print("Thanks for playing!")
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.") # keep looping until valid input
+            game_loop_func()
+
+    # welcome
+    print("\nWelcome to Wordle 2.0!")
+
+    print("\nChoose your difficulty: EASY (E), MEDIUM (M), HARD (H)...") # choose difficulty
+    while True:
+        input_choice = input("Difficulty: ").lower().strip()
+        if input_choice == "e":
+            easy_diff()
+            game_loop_func() # ask to play again
             break
-    else:
-        print(f"\nYou're out of attempts! The word was {word}!") # ran out of attempts
-
-def medium_diff():
-
-    print("Guess the 5 letter word in 6 tries or less...")
-
-    # pick a random word
-    word = random.choice(word_list_five)
-
-    # number of attempts
-    attempts = 1
-
-
-    while attempts <= 6:
-        guess = input(f"\nAttempt {attempts}: ").lower().strip() # ask for user guess
-
-        if len(guess) != 5: # if guess isnt 5 letters
-            print("Your guess must be 5 letters. ") 
-            continue 
-        elif guess.isdigit(): # if guess is a number
-            print("Your guess must not have any numbers! ")
-            continue
-
-
-        attempts += 1 # increase attempt by 1 if valid guess
-
-        guess_result = "" # empty string
-
-        for i in range(5): # go thru each letter in the guess and the word
-            if guess[i] == word[1]:
-                guess_result += "🟩"  # if correct position  
-            elif guess[i] in word:
-                guess_result += "🟨" # incorrect position but word still has letter
-            else:
-                guess_result += "🟥" # word doesnt have letter
-
-        print(guess_result, "\n") # print the boxes 
-
-        if guess == word:
-            print("You guessed the word!") # if u guess the word
+        elif input_choice == "m":
+            medium_diff()
+            game_loop_func()
             break
-    else:
-        print(f"\nYou're out of attempts! The word was {word}!") # ran out of attempts
-
-
-def hard_diff():
-
-    print("Guess the 6 letter word in 6 tries or less...")
-
-    # pick a random word
-    word = random.choice(word_list_six)
-
-    # number of attempts
-    attempts = 1
-
-
-    while attempts <= 6:
-        guess = input(f"\nAttempt {attempts}: ").lower().strip() # ask for user guess
-
-        if len(guess) != 6: # if guess isnt 6 letters
-            print("Your guess must be 5 letters. ") 
-            continue 
-        elif guess.isdigit(): # if guess is a number
-            print("Your guess must not have any numbers! ")
-            continue
-
-
-        attempts += 1 # increase attempt by 1 if valid guess
-
-        guess_result = "" # empty string
-
-        for i in range(6): # go thru each letter in the guess and the word
-            if guess[i] == word[1]:
-                guess_result += "🟩"  # if correct position  
-            elif guess[i] in word:
-                guess_result += "🟨" # incorrect position but word still has letter
-            else:
-                guess_result += "🟥" # word doesnt have letter
-
-        print(guess_result, "\n") # print the boxes 
-
-        if guess == word:
-            print("You guessed the word!") # if u guess the word
+        elif input_choice == "h":
+            hard_diff()
+            game_loop_func()
             break
-    else:
-        print(f"\nYou're out of attempts! The word was {word}!") # ran out of attempts
-
-
-easy_diff()
+        else:
+            print("Invalid input. Please enter 'E', 'M', or 'H'.") # keep looping until valid input
+            continue
